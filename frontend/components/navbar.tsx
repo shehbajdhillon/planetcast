43ee195f05c1a240e2ab3695361a46f807af76ea
dshehbaj <shehbaj.dhillon@gmail.com>
2023-06-01 16:35:12 -0700

    frontend: impl landing page

---
+++ frontend/components/navbar.tsx
@@ -0,0 +1,65 @@
+import {
+  Badge,
+  Box,
+  Button,
+  HStack,
+  Heading,
+  IconButton,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { Moon, Sun } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface NavbarProps {
+  marketing?: boolean;
+};
+
+const Navbar: React.FC<NavbarProps> = ({ marketing }) => {
+
+  const { toggleColorMode } = useColorMode();
+
+  return (
+    <Box w="full" display={"flex"} alignItems={"center"} justifyContent={"center"}>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        w="full"
+        background={useColorModeValue("white", "black")}
+        maxW={"1920px"}
+      >
+        <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
+          <Image
+            src={useColorModeValue('/planetcastlight.svg', '/planetcastdark.svg')}
+            width={60}
+            height={100}
+            alt='spend sense logo'
+          />
+          <Heading
+            fontSize={"30px"}
+            display={{ base: "none", md:"flex" }}
+            fontWeight={"semibold"}
+          >
+            PlanetCast
+          </Heading>
+          <Badge
+            ml="5px"
+          >
+            Beta
+          </Badge>
+        </Box>
+        <HStack>
+          <IconButton
+            onClick={toggleColorMode}
+            aria-label='color mode toggle'
+            icon={useColorModeValue(<Moon />, <Sun />)}
+            variant={"ghost"}
+          />
+        </HStack>
+      </Box>
+    </Box>
+  );
+}
+
+export default Navbar;
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,34 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/react';
+
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const theme = extendTheme({
+  fonts: {
+    heading: inter.style.fontFamily,
+    body: inter.style.fontFamily,
+  },
+  styles: {
+    global: (props: any) => ({
+      body: {
+        bg: mode('white', 'black')(props),
+        textColor: mode('black', 'white')(props),
+      },
+    }),
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </ChakraProvider>
+  );
 }
