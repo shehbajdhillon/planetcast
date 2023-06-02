c610747e38f0d06548a4c3d80f7a09d0d55c2817
dshehbaj <shehbaj.dhillon@gmail.com>
2023-06-01 20:29:01 -0700

    frontend: add cast cards

---
+++ frontend/components/dashboard/cast_card.tsx
@@ -0,0 +1,66 @@
+import {
+  Box,
+  HStack,
+  Spacer,
+  Text,
+  Button,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+interface CastCardProps {
+  title: string;
+  status: "DRAFT" | "PROCESSING" | "DRAFT";
+  totalSteps: number;
+  completedSteps: number;
+};
+
+const CastCard: React.FC<CastCardProps> = (props) => {
+
+  const { title, status, totalSteps, completedSteps } = props;
+
+  const statusColorScheme = status === "DRAFT" ? "gray" : status === "PROCESSING" ? "blue" : "green"
+
+  return (
+    <Box
+      borderWidth={"1px"}
+      p={6}
+      rounded={"lg"}
+      _hover={{
+        borderColor: useColorModeValue('gray.300', 'whiteAlpha.500'),
+        boxShadow: 'lg',
+        bg: useColorModeValue('white', 'whiteAlpha.100'),
+      }}
+      cursor={"pointer"}
+    >
+      <HStack>
+        <Text
+          textTransform="capitalize"
+          fontWeight={700}
+          fontSize={'lg'}
+          letterSpacing={1.1}
+          noOfLines={1}
+        >
+          {title}
+        </Text>
+        <Spacer />
+        <Button
+          borderWidth="1px"
+          size={'xs'}
+          textTransform="capitalize"
+          fontWeight="medium"
+          alignContent="right"
+          colorScheme={statusColorScheme}
+          pointerEvents={"none"}
+        >
+          {status}
+        </Button>
+      </HStack>
+      <Spacer />
+      <HStack pt="20px">
+        <Text>{completedSteps}/{totalSteps} Steps Completed</Text>
+      </HStack>
+    </Box>
+  );
+};
+
+export default CastCard;
--- frontend/components/dashboard/dashboard_tab.tsx
+++ frontend/components/dashboard/dashboard_tab.tsx
@@ -1,8 +1,14 @@
-import { Box, Divider, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Box, Center, Divider, Grid, GridItem, Heading, useBreakpointValue, useColorModeValue } from "@chakra-ui/react";
+import CastCard from "./cast_card";
+import Image from "next/image";
+import useWindowDimensions from "@/hooks/useWindowDimensions";
 
 const DashboardTab: React.FC = () => {
 
   const dividerColor = useColorModeValue("gray.300", "whiteAlpha.300");
+  const imageSize = useBreakpointValue({ base: 70 });
+
+  const { height } = useWindowDimensions();
 
   return (
     <Box w="full" h="full" display={"flex"} flexDir={"column"}>
@@ -10,6 +16,59 @@ const DashboardTab: React.FC = () => {
         <Heading p="5px" fontSize={"3xl"}>Dashboard</Heading>
         <Divider borderColor={dividerColor} />
       </Box>
+      <Center>
+        <Grid
+          px="100px"
+          py={"30px"}
+          placeItems={'center'}
+          columnGap={'25px'}
+          rowGap={'25px'}
+          templateColumns={[
+            'repeat(2, 1fr)',
+            'repeat(2, 1fr)',
+            'repeat(4, 1fr)',
+            'repeat(6, 1fr)',
+          ]}
+          overflow={"auto"}
+          maxH={{ base: (height as number) - 40, lg: height}}
+        >
+          <GridItem colSpan={2} w="full" h="full">
+            <Box
+              borderWidth={"1px"}
+              w="full"
+              h={"full"}
+              p={6}
+              rounded={"lg"}
+              _hover={{
+                borderColor: useColorModeValue('gray.300', 'whiteAlpha.500'),
+                boxShadow: 'lg',
+                bg: useColorModeValue('white', 'whiteAlpha.100'),
+              }}
+              cursor={"pointer"}
+            >
+              <Center h="full" w="full">
+                <Image
+                  src={useColorModeValue('/planetcastlight.svg', '/planetcastdark.svg')}
+                  width={imageSize}
+                  height={100}
+                  style={{ borderRadius: "20px" }}
+                  alt='planet cast logo'
+                />
+              </Center>
+            </Box>
+          </GridItem>
+          {new Array(20).fill(0).map(_ => (
+            <GridItem colSpan={2}>
+              <CastCard
+                title="This Week In Startups"
+                status="DRAFT"
+                totalSteps={6}
+                completedSteps={4}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </Center>
     </Box>
   );
 };
