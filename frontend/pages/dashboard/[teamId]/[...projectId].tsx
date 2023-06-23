ac2c2d1c9ec330719face43e67707cc014ff9c7c
dshehbaj <shehbaj.dhillon@gmail.com>
2023-06-22 19:25:26 -0700

    frontend dashboard: make teamId route

---
+++ frontend/pages/dashboard/[teamId]/[...projectId].tsx
@@ -0,0 +1,35 @@
+import {
+  Box,
+  useColorModeValue,
+  Text,
+} from "@chakra-ui/react";
+import { NextPage } from "next";
+
+import Head from "next/head";
+
+const Dashboard: NextPage = () => {
+  return (
+    <Box>
+      <Head>
+        <title>Project | lanetCast</title>
+        <meta
+          name="description"
+          content="Cast Content in any Language, Across the Planet"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Box position={"fixed"} top={0} left={0} w="full" p="10px" backgroundColor={useColorModeValue("white", "black")} zIndex={1000}>
+        <Text>LMAO</Text>
+      </Box>
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+      >
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
+
