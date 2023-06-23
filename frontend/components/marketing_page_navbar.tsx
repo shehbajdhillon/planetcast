050c940abda3f864aeed5df6117fe37fad6fbe58
dshehbaj <shehbaj.dhillon@gmail.com>
2023-06-23 13:05:06 -0700

    frontend marketing_page: add nprogress to navbar

--- frontend/components/marketing_page_navbar.tsx
+++ frontend/components/marketing_page_navbar.tsx
@@ -12,6 +12,9 @@ import { useUser } from '@clerk/nextjs';
 import { Moon, Sun } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
 
 interface NavbarProps {
   marketing?: boolean;
@@ -21,6 +24,24 @@ const Navbar: React.FC<NavbarProps> = ({ marketing }) => {
 
   const { toggleColorMode } = useColorMode();
   const { isSignedIn, isLoaded } = useUser();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleRouteStart);
+    router.events.on('routeChangeComplete', handleRouteDone);
+    router.events.on('routeChangeError', handleRouteDone);
+
+    return () => {
+      handleRouteDone();
+      router.events.off('routeChangeStart', handleRouteStart);
+      router.events.off('routeChangeComplete', handleRouteDone);
+      router.events.off('routeChangeError', handleRouteDone);
+    };
+  }, []);
 
   return (
     <Box w="full" display={"flex"} alignItems={"center"} justifyContent={"center"}>
