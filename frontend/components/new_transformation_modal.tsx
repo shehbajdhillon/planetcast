2a9f834c440147c2988cd9e0d8c5b9903d9383b2
dshehbaj <shehbaj.dhillon@gmail.com>
2023-07-02 20:29:03 -0700

    frontend new_transformation_modal: add menu

--- frontend/components/new_transformation_modal.tsx
+++ frontend/components/new_transformation_modal.tsx
@@ -1,7 +1,9 @@
-import { Project } from "@/types";
+import { Project, Segment, SupportedLanguage, SupportedLanguages, Transformation } from "@/types";
+import { formatTime } from "@/utils";
 import {
   Box,
   Button,
+  FormLabel,
   IconButton,
   Modal,
   ModalBody,
@@ -10,10 +12,14 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Select,
+  Stack,
+  Text,
+  VStack,
   useDisclosure
 } from "@chakra-ui/react";
 import { PlusIcon } from "lucide-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface NewTransformationModelProps {
   project: Project;
@@ -24,14 +30,16 @@ const NewTransformationModel: React.FC<NewTransformationModelProps> = (props) =>
   const { project } = props;
   const { onOpen, isOpen, onClose } = useDisclosure();
 
-  const sourceTransformation = project?.transformations?.find(t => t.isSource);
+  const sourceTransformation: Transformation | undefined  = project?.transformations?.find(t => t.isSource);
+  const sourceTranscript = sourceTransformation?.transcript && JSON.parse(sourceTransformation.transcript)
+  const sourceSegments: Segment[] = sourceTranscript?.segments;
+  const [targetLanguage, setTargetLanguage] = useState<SupportedLanguage>();
 
   useEffect(() => {
-    console.log({ sourceTransformation });
-  }, [sourceTransformation]);
+    setTargetLanguage(undefined);
+  }, [isOpen]);
 
   return (
-
     <Box>
       <IconButton aria-label="add new dubbing" icon={<PlusIcon />} variant={"ghost"} onClick={onOpen} />
       <Modal isOpen={isOpen} onClose={onClose} size={"6xl"} isCentered>
@@ -40,7 +48,56 @@ const NewTransformationModel: React.FC<NewTransformationModelProps> = (props) =>
           <ModalHeader>New Dubbing</ModalHeader>
           <ModalCloseButton />
 
-          <ModalBody>
+          <ModalBody overflow={"auto"} maxH={"80vh"}>
+            <Stack w="full" h={"full"} direction={"row"}>
+
+              <VStack w="50%">
+                <Button variant={"outline"} pointerEvents={"none"}>{sourceTransformation?.targetLanguage}</Button>
+                <VStack>
+                  {sourceSegments.map((segment: Segment, idx: number) => (
+                    <Button
+                      key={idx}
+                      rounded="10px"
+                      whiteSpace={'normal'}
+                      height="auto"
+                      blockSize={'auto'}
+                      w="full"
+                      justifyContent="left"
+                      leftIcon={<Text>{formatTime(segment.start)} - {formatTime(segment.end)}</Text>}
+                      variant={'outline'}
+                    >
+                      <Text
+                        key={idx}
+                        textAlign={"left"}
+                        padding={2}
+                      >
+                        { segment.text.trim() }
+                      </Text>
+                    </Button>
+                  ))}
+                </VStack>
+              </VStack>
+
+              <Box mx="auto">
+                <Stack spacing={2}>
+                  <FormLabel
+                    fontWeight={'600'}
+                    fontSize={'lg'}
+                  >
+                    Target Language
+                  </FormLabel>
+                  <Select value={targetLanguage} onChange={(e) => setTargetLanguage((e.target.value as SupportedLanguage))}>
+                    {SupportedLanguages.map((lang, idx) => (
+                      <option key={idx} value={lang}>{lang}</option>
+                    ))}
+                  </Select>
+                  <Button hidden={!targetLanguage}>
+                    Submit
+                  </Button>
+                </Stack>
+              </Box>
+
+            </Stack>
           </ModalBody>
 
           <ModalFooter w="full">
